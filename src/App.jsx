c33d7b0a46c5330b8281faa700f591027cdacead
Isaac Zimba <161163938+ZimbaIsaac@users.noreply.github.com>
2024-03-03 16:08:33 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Isaac Zimba",
+  title: "Software Engineer",
+  email: "isaacjunior.zimba@studentambassadors.com",
+  gitHub: "https://github.com/ZimbaIsaac",
+  instagram: " ",
+  linkedIn: "www.linkedin.com/in/isaac-zimba-842061239",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
